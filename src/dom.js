e6a9781e87ef86252b6c5b842057a3e05898c7de
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-04-09 20:08:33 +0300

    Edit dom functions & add comments

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,7 @@
+import { Player } from "./player.js";
+
 const dom = (() => {
+    //create 10x10 grid elements
     const createGrid = () => {
         const gridContainer = document.createElement('div');
         gridContainer.className = 'grid-container';
@@ -11,41 +14,32 @@ const dom = (() => {
             while (rows < 10) {
                 let element = document.createElement('div');
                 element.setAttribute('data-id', ([rows, cols]));
+                element.id = [rows, cols];
                 grid.appendChild(element);
                 rows ++;
             }
             cols++;
             rows = 0;
         }
-/*        for (let i = 0; i < 10; i++) {
-            let gridRow = document.createElement('div');
-            gridRow.setAttribute('data-id', ([i, j]));
-            grid.appendChild(gridRow);
-            for (let j = 0; j < 10; j++) {
-                let gridCol = document.createElement('div');
-                gridCol.setAttribute('data-id', ([i, j]));
-                grid.appendChild(gridCol);
-            }
-        }*/
-
         gridContainer.appendChild(grid);
         return gridContainer;
     }
-    const createShips = () =>{
+    //create elements for ships with id linked to related player
+    const createShips = (player) =>{
         const shipContainer = document.createElement('div');
         shipContainer.className = 'ship-container';
 
-        const carrier = document.createElement('p');
-        const battleship = document.createElement('p');
-        const cruiser = document.createElement('p');
-        const submarine = document.createElement('p');
-        const destroyer = document.createElement('p');
+        const carrier = document.createElement('div');
+        const battleship = document.createElement('div');
+        const cruiser = document.createElement('div');
+        const submarine = document.createElement('div');
+        const destroyer = document.createElement('div');
 
-        carrier.id = 'carrier';
-        battleship.id = 'battleship';
-        cruiser.id = 'cruiser';
-        submarine.id = 'submarine';
-        destroyer.id = 'destroyer';
+        carrier.id = 'carrier' + player;
+        battleship.id = 'battleship'+ player;
+        cruiser.id = 'cruiser'+ player;
+        submarine.id = 'submarine'+ player;
+        destroyer.id = 'destroyer'+ player;
         shipContainer.appendChild(carrier);
         shipContainer.appendChild(battleship);
         shipContainer.appendChild(cruiser);
@@ -53,19 +47,33 @@ const dom = (() => {
         shipContainer.appendChild(destroyer);
         return shipContainer;
     }
-    const players = () => {
+    // create elements for players
+    const players = (id) => {
         const playerContainer = document.createElement('div');
         playerContainer.className = 'player-container';
 
         const player = document.createElement('p');
-        player.id = 'player';
+        player.id = 'player' + id;
 
         const element = document.querySelector('.player-container');
         element.appendChild(player);
         return element;
     }
-
-    return {createGrid, createShips, players}
+    // display gameboard array from selected player in the grid
+    const displayGrid = (player) => {
+        let rows = 0;
+        let cols = 0;
+        while (cols < 10) {
+            while (rows < 10) {
+                let gridElement = document.getElementById([rows, cols]);
+                gridElement.textContent = player.gameboard.board[cols][rows];
+                rows ++;
+            }
+            cols++;
+            rows = 0;
+        }
+    }
+    return {createGrid, createShips, players, displayGrid}
 })();
 
 export { dom };
